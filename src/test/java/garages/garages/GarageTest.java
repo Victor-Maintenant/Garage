764b80bfbb5bb0c0052bf977c5e2fc4f2711adce
Victor <victor.maintenant@etud.univ-jfc.fr>
2020-11-17 16:41:52 +0100

    Initilisation du projet dans Eclipse, réglage de problèmes.

--- src/test/java/garages/garages/GarageTest.java
+++ src/test/java/garages/garages/GarageTest.java
@@ -1,4 +1,4 @@
-package garages;
+package garages.garages;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -8,6 +8,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import garages.Garage;
+import garages.Voiture;
 
 public class GarageTest {
 
@@ -23,7 +25,7 @@ public class GarageTest {
 
 	@Test
 	public void testInitialisationVoiture() {
-		// Au début, la voiture n'est pas dans un garage
+		// Au dÃ©but, la voiture n'est pas dans un garage
 		assertFalse(v1.estDansUnGarage());
 		assertTrue(v1.garagesVisites().isEmpty());
 	}
@@ -32,9 +34,9 @@ public class GarageTest {
 	public void testEntreeGarage() throws Exception {
 		// On fait entrer la voiture au garage g1
 		v1.entreAuGarage(g1);
-		// Elle doit être dans un garage
+		// Elle doit Ãªtre dans un garage
 		assertTrue(v1.estDansUnGarage());
-		// g1 fait partie des garages visités par la voiture
+		// g1 fait partie des garages visitÃ©s par la voiture
 		assertTrue(v1.garagesVisites().contains(g1));
 	}
 
@@ -44,7 +46,7 @@ public class GarageTest {
 		v1.sortDuGarage();
 		// Elle n'est plus dans un garage
 		assertFalse(v1.estDansUnGarage());
-		// g1 fait partie des garages visités par la voiture
+		// g1 fait partie des garages visitÃ©s par la voiture
 		assertTrue(v1.garagesVisites().contains(g1));
 	}
 
@@ -54,7 +56,7 @@ public class GarageTest {
 		v1.sortDuGarage(); // Ici il ne doit pas y avoir d'exception
 		try {
 			v1.sortDuGarage(); // Que doit-il se passer ?
-			// Si on arrive ici, il n'y a pas eu d'exception, échec
+			// Si on arrive ici, il n'y a pas eu d'exception, Ã©chec
 			fail();
 		} catch (Exception e) {
 			// Si on arrive ici, il y a eu une exception, c'est ce qui est attendu
@@ -66,7 +68,7 @@ public class GarageTest {
 		v1.entreAuGarage(g1);
 		try {
 			v1.entreAuGarage(g2); // Que doit-il se passer ?
-			// Si on arrive ici, il n'y a pas eu d'exception, échec
+			// Si on arrive ici, il n'y a pas eu d'exception, Ã©chec
 			fail();
 		} catch (Exception e) {
 			// Si on arrive ici, il y a eu une exception, c'est ce qui est attendu
@@ -74,14 +76,14 @@ public class GarageTest {
 	}
 
 	/**
-	 * Exemple de test qui vérifie un format d'impression correct.<br>`
-	 * La méthode "imprimeStationnements" est conçue pour être testable :<br>
-	 * Elle prend un paramètre "PrintStream" qui indique où on doit imprimer
-	 * (Injection de dépendance).<br>
-	 * Dans le test, on imprime dans une chaîne de caractères au lieu d'imprimer
+	 * Exemple de test qui vÃ©rifie un format d'impression correct.<br>`
+	 * La mÃ©thode "imprimeStationnements" est conÃ§ue pour Ãªtre testable :<br>
+	 * Elle prend un paramÃ¨tre "PrintStream" qui indique oÃ¹ on doit imprimer
+	 * (Injection de dÃ©pendance).<br>
+	 * Dans le test, on imprime dans une chaÃ®ne de caractÃ¨res au lieu d'imprimer
 	 * directement dans la console (System.out)<br>
-	 * On peut ensuite vérifier que le contenu de la chaîne générée est conforme au
-	 * résultat attendu.
+	 * On peut ensuite vÃ©rifier que le contenu de la chaÃ®ne gÃ©nÃ©rÃ©e est conforme au
+	 * rÃ©sultat attendu.
 	 * @throws Exception 
 	 */
 	@Test
@@ -92,20 +94,20 @@ public class GarageTest {
 		v1.sortDuGarage();
 		v1.entreAuGarage(g1);
 
-		// La méthode imprimeGarages va écrire dans une chaine de caractères
+		// La mÃ©thode imprimeGarages va Ã©crire dans une chaine de caractÃ¨res
 		ByteArrayOutputStream os = new ByteArrayOutputStream();
 		PrintStream ps = new PrintStream(os);
 		// On imprime dans os
 		v1.imprimeStationnements(ps);
 		
-		// On récupère le résultat de l'impression
+		// On rÃ©cupÃ¨re le rÃ©sultat de l'impression
 		String output = os.toString("UTF8");
 
 		assertEquals(1, countSubstring(output, g1.toString()),
-                         g1.toString() + " doit apparaître une fois");
+                         g1.toString() + " doit apparaÃ®tre une fois");
 
 		assertEquals(1,	countSubstring(output, g2.toString()),
-                    g2.toString() + " doit apparaître une fois");
+                    g2.toString() + " doit apparaÃ®tre une fois");
 
 		assertEquals(3,	countSubstring(output, "Stationnement"),
                         "On doit imprimer trois stationnements"	);
@@ -115,8 +117,8 @@ public class GarageTest {
 	}
 
 	/**
-	 * Une méthode utilitaire pour le test ci-dessus
-	 * Compter le nombre d'occurrences d'une sous-chaîne dans une chaîne
+	 * Une mÃ©thode utilitaire pour le test ci-dessus
+	 * Compter le nombre d'occurrences d'une sous-chaÃ®ne dans une chaÃ®ne
 	 *
 	 * @param string String to look for substring in.
 	 * @param substring Sub-string to look for.
